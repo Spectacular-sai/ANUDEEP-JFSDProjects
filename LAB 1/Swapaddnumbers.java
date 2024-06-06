d612465bf8e87f6b605e7aa45448621e5566558d
SAI KUMAR GUDIBOINA <saikumargudiboina99@gmail.com>
2024-06-07 01:38:24 +0530

    Add files via upload

---
+++ LAB 1/Swapaddnumbers.java
@@ -0,0 +1,30 @@
+package saijavafullstackdevelopement;
+
+public class Swapaddnumbers {
+                        
+	           public static void main(String[] args) {
+	        	        // TODO Auto-generated method stub
+	        	   
+	          int x=5;
+	          int y=9;
+	          
+	          System.out.println("Before swapping");
+	          System.out.println("+x:"+x);
+	          System.out.println("+y:"+y);
+	          
+	          int z=x;
+	          x=y;
+	          y=z;
+	    
+	          System.out.println("After swapping");
+	          System.out.println("+x:"+x);
+	          System.out.println("+y:"+y);
+	          
+	          int sum = x + y;
+	          
+	          System.out.println("Sum of x and y: " + sum);
+	           }
+}
+
+
+ 
